4299044efb69fc23e2a2916e525cb42c76a6f54c
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-09-28 12:05:29 +0300

    Add hot_chick

--- src/main.py
+++ src/main.py
@@ -5,6 +5,12 @@ from telebot.types import Message
 def on_start(message: Message) -> None:
     bot.send_message(chat_id=message.from_user.id,
                      text='Hello, *Sweety* 💋!')
+    
+@bot.message_handler(commands=['hot_chick'])
+def on_hot_chick(message: Message) -> None:
+    with open(file='hot_chick.jpg', mode='rb') as hot_chick_photo:
+        bot.send_photo(chat_id=message.from_user.id,
+                        photo=hot_chick_photo)
 
 if __name__ == '__main__':
     bot.infinity_polling()
